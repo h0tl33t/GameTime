9a702fa4d4d69d177ba3aa53aecf128ddc3db3c8
Ryan Stenberg <h0tl33t@gmail.com>
2013-05-20 15:16:02 -0400

    Add home page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ GameTime::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'home#index'
+  
+  get 'home/index' => 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
