a0d4ae6b108e0177d620e572c85c74c0bae9c8d7
Ryan Stenberg <h0tl33t@gmail.com>
2013-05-20 12:12:47 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameTime::Application.config.secret_key_base = '450b1fa2ab8796890292b63a5d51a7209d06cd32813691cd28082632d4d2989449ac3f3baea877baf0d3494466c1ebae4e406b037a23d84279dfc95415f956a9'
