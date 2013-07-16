763b4b8bdc240146978ec728fd4c5ae93f208481
Ryan Stenberg <h0tl33t@gmail.com>
2013-07-16 07:38:46 -0400

    Fix for pending friendship requests persisting after being accepted.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,7 +6,7 @@ class FriendshipsController < ApplicationController
 			if current_player.mutual_friends.include?(@friendship.friend)
 				mutual_friend = @friendship.friend
 				@friendship.update_attributes(pending: false, mutual: true)
-				mutual_friend.friendships.select{|friendship| friendship.friend = current_player}.first.update_attributes(pending: false, mutual: true)
+				mutual_friend.friendships.select{|friendship| friendship.friend == current_player}.first.update_attributes(pending: false, mutual: true)
 			else
 				GametimeMailer.pending_friend_request(@friendship.friend, current_player).deliver
 			end
